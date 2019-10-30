af0a3bedb4172a92f3e79f20559f3b52c048d003
george.pomaskin <Pomaskin@utip.ru>
2019-10-30 14:36:19 +0500

    Added NTFS junction between server and client.

--- Server/server.ts
+++ Server/server.ts
@@ -2,14 +2,13 @@ import express from 'express';
 
 export default function setupHttpServer(): void {
     const app = express();
-    const port = 8080; // default port to listen
-
-    // define a route handler for the default home page
-    app.get('/', (req, res) => {
-        res.send('Hello world!');
-    });
+    const port = 8080;
 
     app.use(express.static('public'));
+
+    app.post('/connection-info/new', (req, res) => {
+        // TODO
+    });
 
     app.listen(port, () => {
         console.log(`server started at http://localhost:${port}`);
